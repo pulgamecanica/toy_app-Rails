3124b76c61b7e1823cc58a3ed85ce049712a0346
codeamac06 <codea_mac_06@CodeaCamp06s-Mac-mini.local>
2017-07-25 17:02:05 -0500

    Final Chapter3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
 
   resources :microposts
   resources :users
-  root 'application#hello'
+  root 'static_pages#home'
 end
